ad54492b9c588331800300256916c989d6aca9ec
Nikolas Förster <foerster@lea.uni-paderborn.de>
2021-04-06 20:54:20 +0200

    update_from_fileexchange: rm access token for file-exchange due to public repository from now on

--- transistordatabase/databaseClasses.py
+++ transistordatabase/databaseClasses.py
@@ -258,20 +258,21 @@ class Transistor:
             TypeError(f"{path = } ist not a string.")
 
     @staticmethod
-    def update_from_fileexchange(access_token, collection="local", overwrite=True):
+    def update_from_fileexchange(collection="local", overwrite=True):
         if collection == "local":
             collection = Transistor.connect_local_TBD()
-        repo_url = f"https://{access_token}:x-oauth-basic@github.com/upb-lea/transistordatabase_File_Exchange"
+        repo_url = f"https://github.com/upb-lea/transistordatabase_File_Exchange"
         local_dir = "./cloned_repo"
         Repo.clone_from(repo_url, local_dir)
         for subdir, dirs, files in os.walk(local_dir):
             for file in files:
-                # print os.path.join(subdir, file)
+                # print(f"{os.path.join(subdir, file)}")
                 filepath = subdir + os.sep + file
 
                 if filepath.endswith(".json"):
                     transistor = Transistor.import_json(filepath)
                     transistor.save(collection, overwrite)
+                    print(f"Update Transistor: {transistor.name}")
 
         for root, dirs, files in os.walk(local_dir):
             for dir in dirs:
