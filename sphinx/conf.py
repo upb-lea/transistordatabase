165477e14098906c02f83d8be1bd0874c8a5f867
Nikolas Förster <foerster@lea.uni-paderborn.de>
2021-09-16 16:34:49 +0200

    bump doc version to 0.2.12

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, LEA, Paderborn University'
 author = 'LEA-UPB'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.11'
+release = '0.2.12'
 
 # -- General configuration ---------------------------------------------------
 
